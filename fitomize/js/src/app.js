1b360f4c7bea6789b2e3ce56d6d5535c5cc0db33
Lisa Ross <lisa@thisisbroad.com>
2019-04-24 13:34:14 -0400

    editor configs and linting

--- fitomize/js/src/app.js
+++ fitomize/js/src/app.js
@@ -1,1 +1,1 @@
-const fn = () => 1;
+const fn = () => 1
--- fitomize/webpack.config.js
+++ fitomize/webpack.config.js
@@ -1,53 +1,59 @@
-const path = require('path');
+const path = require('path')
 
 // include the js minification plugin
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 // include the css extraction and minification plugins
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-	entry: ['./js/src/app.js', './css/src/app.scss'],
-	output: {
-		filename: './js/build/app.min.js',
-		path: path.resolve(__dirname)
-	},
-	module: {
-		rules: [
-			// perform js babelization on all .js files
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ['@babel/env']
-					}
-				}
-			},
-			 // compile all .scss files to plain old css
-      		{
-				test: /\.(sass|scss)$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	plugins: [
-		// extract css into dedicated file
-		new MiniCssExtractPlugin({
-			filename: './css/build/main.min.css'
-		})
-	],
-	optimization: {
-		minimizer: [
-			// enable the js minification plugin
-			new UglifyJSPlugin({
-				cache: true,
-				parallel: true
-			}),
-			// enable the css minification plugin
-			new OptimizeCSSAssetsPlugin({})
-		]
-	}
-};
+  entry: ['./js/src/app.js', './css/src/app.scss'],
+  output: {
+    filename: './js/build/app.min.js',
+    path: path.resolve(__dirname)
+  },
+  module: {
+    rules: [
+      // perform js babelization on all .js files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        }
+      },
+      // compile all .scss files to plain old css
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // extract css into dedicated file
+    new MiniCssExtractPlugin({
+      filename: './css/build/main.min.css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      // enable the js minification plugin
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      }),
+      // enable the css minification plugin
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
+}
