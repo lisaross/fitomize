e20e96c89fa523e779b3b21189f33c29a1ba32c0
Lisa Ross <lisa@thisisbroad.com>
2019-04-24 12:31:37 -0400

    change src and build dirs for js

--- fitomize/webpack.config.js
+++ fitomize/webpack.config.js
@@ -1,23 +1,36 @@
 const path = require('path');
 
+// include the js minification plugin
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
-	entry: './src/index.js',
+	entry: ['./js/src/app.js'],
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		filename: './js/build/app.min.js',
+		path: path.resolve(__dirname)
 	},
 	module: {
 		rules: [
+			// perform js babelization on all .js files
 			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
+				test: /\.js$/,
+				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
-						presets: ['@babel/preset-env']
+						presets: ['@babel/env']
 					}
 				}
 			}
+		]
+	},
+	optimization: {
+		minimizer: [
+			// enable the js minification plugin
+			new UglifyJSPlugin({
+				cache: true,
+				parallel: true
+			})
 		]
 	}
 };
